86f4a499fe405231b7743d476d9c180222f66abe
Rogério Costa <57320699+rogerio-costa@users.noreply.github.com>
2021-08-05 11:45:24 -0300

    Atualizações

---
+++ src/config/googleAuth.tsx
@@ -0,0 +1,2 @@
+export const IOS_CLIENT_ID = "655525004701-b37ql1iq8gp8u41ir8f54jj2u921q2db.apps.googleusercontent.com";
+export const ANDROID_CLIENT_ID = "655525004701-h4m2rtl6i4hhvi4hkmi1eeuadmagjuoa.apps.googleusercontent.com";
